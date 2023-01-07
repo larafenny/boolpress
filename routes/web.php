97182a2deb85a201c4af7ebeb4e9bca71ac2049d
Lara Filippone <lara.filippone@gmail.com>
2023-01-07 16:22:25 +0100

    created routs admin and guest

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+// Rotta per l'autenticazione gestita automaticamente da Laravel
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')
+    //se si va a qualunque indirizzo .admin torna alla pagina di login
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    //se si è autenticato va alla home di admin
+    ->group(function() {
+        Route::get('/', 'HomeController@index')
+        ->name('home');
+    });
+
+// se non ci sono altre rotte allora va in guest.home
+Route::get('{any?}', function(){
+    return view('guest.home');
+
+})->where('any', '.*');
